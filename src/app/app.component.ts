8b04c08919cbeb8af4926e9ca4354f37e667e92d
Renzo Fraga <kenkomarinho@gmail.com>
2024-07-11 14:30:43 -0300

    update firebase
    update firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceapiService } from './services/faceapi.service';
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,20 @@ import { FaceapiService } from './services/faceapi.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  
   constructor(private faceApiService: FaceapiService) {}
 
   async ngOnInit() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDLUP7YYdCHW7Nm14y3I1_TStnPMUXEAik",
+      authDomain: "vepema-demo-leds-897bc.firebaseapp.com",
+      projectId: "vepema-demo-leds",
+      storageBucket: "vepema-demo-leds.appspot.com",
+      messagingSenderId: "946008911110",
+      appId: "1:946008911110:web:50df7ae70920452f97c287"
+    };
+    const app = initializeApp(firebaseConfig);
+    
     await this.faceApiService.loadModels();
   }
 }
